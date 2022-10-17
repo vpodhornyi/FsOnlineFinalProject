38930056b1bd94b6752a31f92ecc5bafe2678885
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2022-10-17 20:46:03 +0300

    add checkstyle_config.xml

--- src/main/java/com/twitter/danit/FsOnlineFinalProjectApplication.java
+++ src/main/java/com/twitter/danit/FsOnlineFinalProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FsOnlineFinalProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FsOnlineFinalProjectApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(FsOnlineFinalProjectApplication.class, args);
+  }
 
 }
