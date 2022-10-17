7a18c3e8c88857c3bd2c51aa414d5ffa3d54c3fe
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2022-10-17 19:06:53 +0300

    feature/server_base: init

---
+++ src/main/java/com/twitter/danit/FsOnlineFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.danit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsOnlineFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsOnlineFinalProjectApplication.class, args);
+	}
+
+}
