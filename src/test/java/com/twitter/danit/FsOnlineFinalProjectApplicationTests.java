6366a7e843f6922eb949d9cdf9cb786dfbf5f23c
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2022-10-19 22:49:03 +0300

    add server false test

--- src/test/java/com/twitter/danit/FsOnlineFinalProjectApplicationTests.java
+++ src/test/java/com/twitter/danit/FsOnlineFinalProjectApplicationTests.java
@@ -3,11 +3,16 @@ package com.twitter.danit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
 class FsOnlineFinalProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		boolean foo = true;
+		assertFalse(foo);
 	}
 
 }
