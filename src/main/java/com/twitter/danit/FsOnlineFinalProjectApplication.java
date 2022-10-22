2817a7b6ef2c2d793bb1df9a89b56028fbe93145
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2022-10-22 13:02:50 +0300

    fix bahs again

--- src/main/java/com/twitter/danit/FsOnlineFinalProjectApplication.java
+++ src/main/java/com/twitter/danit/FsOnlineFinalProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FsOnlineFinalProjectApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(FsOnlineFinalProjectApplication.class, args)
+    SpringApplication.run(FsOnlineFinalProjectApplication.class, args);
   }
 
 };
