0751e8282d40c60efc6aabd7538da96d3cdd6530
Dariy Pavlyk <dariypavlyk@gmail.com>
2022-10-23 20:46:45 +0200

    container styling fixed

--- client/src/components/MainContainer/index.jsx
+++ client/src/components/MainContainer/index.jsx
@@ -6,13 +6,11 @@ import Main from "@pages/Main";
 const MainContainer = (props) => {
   return (
     <Grid container justifyContent="center" spacing={0}>
-      <Grid item md={7} s={7} xs={7}>
-        <Container>Here goes the primary column with twits</Container>
+      <Grid item md={7} s={7} xs={7} height="100vh">
+        Here goes the primary column with twits
       </Grid>
-      <Grid item md={5} s={5} xs={5}>
-        <Container>
-          <Container>Here goes the right sidebar</Container>
-        </Container>
+      <Grid item md={5} s={5} xs={5} height="100vh">
+        <Container>Here goes the right sidebar</Container>
       </Grid>
     </Grid>
   );
--- client/src/pages/Main/index.jsx
+++ client/src/pages/Main/index.jsx
@@ -6,13 +6,11 @@ const Main = () => {
   return (
     <>
       <Grid container justifyContent="center" spacing={0}>
-        <Grid item md={3} s={3} xs={3}>
+        <Grid item md={3} s={3} xs={3} height="100vh">
           <Container>Here goes the left sidebar/header </Container>
         </Grid>
-        <Grid item md={9} s={9} xs={9}>
-          <Container>
-            <MainContainer />
-          </Container>
+        <Grid item md={9} s={9} xs={9} height="100vh">
+          <MainContainer />
         </Grid>
       </Grid>
     </>
