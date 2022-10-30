77486b2a387c6620e3a658c12e4d1e82f8922ea3
Dariy Pavlyk <dariypavlyk@gmail.com>
2022-10-30 22:43:32 +0100

    theme refactored and moved to a separate file

--- client/src/index.js
+++ client/src/index.js
@@ -8,24 +8,10 @@ import Auth from "./pages/Auth";
 import "normalize.css";
 import { ThemeProvider } from "@emotion/react";
 import { createTheme } from "@mui/material/styles";
-
+import { defaultTheme } from "@utils/defaultTheme";
 const reduxStore = store();
 const root = createRoot(document.getElementById("root"));
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 0,
-      sm: 500,
-      // left container dissapears when < 500
-      md: 988,
-      // right container dissapears when < 988
-      lg: 1077,
-      // messages icon tab dissapears from bottom right side when <  1077
-      xl: 1265,
-      // left container becomes compressed when < 1265
-    },
-  },
-});
+const theme = createTheme(defaultTheme);
 root.render(
   <Provider store={reduxStore}>
     <ThemeProvider theme={theme}>
