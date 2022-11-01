3ee7f21fd7952c73defb81ecb7bc2a1b84ad6933
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2022-11-02 00:20:49 +0200

    add adaptive reply component, fix bags with adaptive tweet component,remove unused button, change part css tweet component

--- client/src/components/Reply/Reply.jsx
+++ client/src/components/Reply/Reply.jsx
@@ -2,41 +2,34 @@ import React from "react";
 import Tweet from "../Tweet";
 import IconButton from "@mui/material/IconButton";
 import Dialog from "@mui/material/Dialog";
-import Button from "@mui/material/Button";
 import css from "./style.module.scss";
 import { CloseIcon } from "../../media/icons";
 import Box from "@mui/material/Box";
 import { TweetForm } from "../TweetForm";
-const Reply = () => {
-  const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+import PropTypes from "prop-types";
+import useMediaQuery from "@mui/material/useMediaQuery";
+const Reply = ({ open = false, handleClose }) => {
+  const matches = useMediaQuery("(max-width:700px)");
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open dialog
-      </Button>
-      <Dialog
-        className={css.wrapper}
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
-        open={open}
-      >
-        <Box>
-          <IconButton aria-label="close" onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
-        </Box>
-        <Tweet openModal={true} />
-        <TweetForm buttonText={"Reply"} placeholderText={"Tweet your reply"} />
-      </Dialog>
-    </div>
+    <Dialog
+      fullScreen={matches}
+      className={css.wrapper}
+      onClose={handleClose}
+      aria-labelledby="customized-dialog-title"
+      open={open}
+    >
+      <Box>
+        <IconButton aria-label="close" onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+      </Box>
+      <Tweet openModal={true} />
+      <TweetForm buttonText={"Reply"} placeholderText={"Tweet your reply"} />
+    </Dialog>
   );
 };
-
+Reply.propTypes = {
+  open: PropTypes.bool,
+  handleClose: PropTypes.func,
+};
 export default Reply;
--- client/src/components/TweetForm/styles.js
+++ client/src/components/TweetForm/styles.js
@@ -1,14 +1,14 @@
-import {styled} from '@mui/material/styles';
-import {Box, Button, List, ListItem} from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Box, Button, List, ListItem } from "@mui/material";
 
 export const TwitterContainer = styled(Box)({
-  fontFamily: 'Arial',
+  fontFamily: "Arial",
   paddingTop: 5,
   paddingBottom: 10,
   paddingLeft: 16,
   paddingRight: 16,
-  borderBottom: '1px solid #eff3f4',
-  display: 'flex',
+  borderBottom: "1px solid #eff3f4",
+  display: "flex",
 });
 
 export const AvatarContainer = styled(Box)({
@@ -17,53 +17,58 @@ export const AvatarContainer = styled(Box)({
 });
 
 export const Form = styled(Box)({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '95%',
+  display: "flex",
+  flexDirection: "column",
+  width: "95%",
 });
 
 export const TweetInput = styled(Box)({
-  borderBottom: '1px solid #eff3f4',
+  borderBottom: "1px solid #eff3f4",
 });
 
 export const FormFooter = styled(Box)({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
 });
 
 export const IconsList = styled(List)({
   marginTop: 15,
-  display: 'inline-flex',
+  display: "inline-flex",
 });
 
 export const Icon = styled(ListItem)({
   width: 20,
   height: 20,
-  color: 'rgb(29, 155, 240)',
+  color: "rgb(29, 155, 240)",
   marginRight: 10,
 });
 
 export const ReplyText = styled(Box)({
   paddingBottom: 15,
-  display: 'flex',
-  alignItems: 'center',
-  color: 'rgb(29, 155, 240)',
+  display: "flex",
+  alignItems: "center",
+  color: "rgb(29, 155, 240)",
   fontSize: 14,
   fontWeight: 700,
   marginTop: 15,
-})
+});
 
 export const TweetBtn = styled(Button)({
-  color: '#fff',
-  backgroundColor: 'rgb(29, 155, 240)',
-  padding: '0 20',
-  justifySelf: 'center',
+  color: "#fff",
+  backgroundColor: "rgb(29, 155, 240)",
+  padding: "0 20",
+  justifySelf: "center",
   borderRadius: 30,
   height: 35,
   fontWeight: 600,
   fontSize: 14,
-  cursor: 'pointer',
+  cursor: "pointer",
   border: 0,
-  textTransform: 'inherit',
+  textTransform: "inherit",
+  ["@media (max-width:700px)"]: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+  },
 });
