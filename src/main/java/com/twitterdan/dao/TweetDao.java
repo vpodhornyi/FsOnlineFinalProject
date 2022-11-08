d870daa4ebbab21db0e9f71d8024ed476a58ead9
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2022-11-08 23:09:44 +0200

    fix bags with JpaRepository, add data.sql

--- src/main/java/com/twitterdan/dao/TweetDao.java
+++ src/main/java/com/twitterdan/dao/TweetDao.java
@@ -2,9 +2,8 @@ package com.twitterdan.dao;
 
 import com.twitterdan.domain.tweet.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-public interface TweetDao extends JpaRepository<Tweet, Long> {
-    Optional<Tweet> findAccountByNumber(String accountNumber);
+@Repository
+public interface TweetDao extends JpaRepository<Tweet,Long> {
 }
