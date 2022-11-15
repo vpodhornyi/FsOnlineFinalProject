070ff8ff03e96cdb1ce60f5663e279873303dbb4
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2022-11-15 14:07:08 +0200

    messages: import store ref

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
+import store from "../src/redux/store";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "@containers/AppContainer";
-import store from "../src/redux/store";
 import { BrowserRouter } from "react-router-dom";
 import "./normolize.css";
 
