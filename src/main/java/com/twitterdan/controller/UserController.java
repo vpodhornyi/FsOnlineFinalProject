f71a6a43668f8952aab683697bd6db5016918989
Lobachevskyi Maksym <lobachevskiy.maksim@gmail.com>
2022-11-16 01:19:33 +0100

    Update UserController.java

--- src/main/java/com/twitterdan/controller/UserController.java
+++ src/main/java/com/twitterdan/controller/UserController.java
@@ -6,7 +6,10 @@ import com.twitterdan.facade.user.UserRequestMapper;
 import com.twitterdan.facade.user.UserResponseMapper;
 import com.twitterdan.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
