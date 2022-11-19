52bdc8f0c638e9ec29d2b3ea17e5721cdad52ab4
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2022-11-19 15:20:47 +0200

    messages_endpoints: DialogNeMessage ref

--- client/src/pages/Messages/components/DialogNewMessage/index.jsx
+++ client/src/pages/Messages/components/DialogNewMessage/index.jsx
@@ -3,18 +3,7 @@ import {useDispatch} from "react-redux";
 import {styled} from "@mui/material/styles";
 import {Box, TextField} from "@mui/material";
 import Header from "./Header";
-
-
-const styles = ({theme}) => ({
-  width: '580px',
-  height: '95%',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start'
-});
-
-
-const BoxWrapper = styled(Box)(styles);
+import IconByName from "@components/icons/IconByName";
 
 const Index = () => {
   const dispatch = useDispatch();
@@ -22,8 +11,45 @@ const Index = () => {
   return (
     <BoxWrapper>
       <Header/>
+      <IconByName iconName='SearchOutlined'/>
       <TextField id="standard-basic" placeholder="Search people" variant="standard" fullWidth/>
     </BoxWrapper>);
 }
+
+const styles = ({theme}) => ({
+  width: '100%',
+  height: '100%',
+
+  '& > .MuiSvgIcon-root': {
+    position: 'absolute',
+    top: 73,
+    left: 17,
+  },
+
+  '& .MuiInputBase-root': {
+    paddingLeft: 50,
+    paddingBottom: 5,
+    borderBottom: '1px solid #DDDFE2',
+
+    '&:before': {
+      content: 'none'
+    },
+
+    '&:after': {
+      content: 'none'
+    }
+  },
+
+  [theme.breakpoints.up(700)]: {
+    width: '580px',
+    height: '50vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start'
+  }
+});
+
+
+const BoxWrapper = styled(Box)(styles);
 
 export default Index;
