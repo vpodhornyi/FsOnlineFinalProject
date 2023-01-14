efeeb8d6bd9ebc1da70fe35af78c99f9a4d00d5f
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2023-01-15 00:22:56 +0200

    new_routes_messages: messages seen ref

--- client/src/pages/Messages/components/Chat/Message/UnreadMessagesNotification.jsx
+++ client/src/pages/Messages/components/Chat/Message/UnreadMessagesNotification.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import {useSelector, useDispatch} from "react-redux";
 import {styled} from "@mui/material/styles";
 import {Box, Typography} from "@mui/material";
+import PropTypes from "prop-types";
 
-const UnreadMessagesNotification = () => {
+const UnreadMessagesNotification = ({item}) => {
+  const dispatch = useDispatch();
 
   return (
     <BoxWrapper>
@@ -12,12 +15,15 @@ const UnreadMessagesNotification = () => {
 
 const BoxWrapper = styled(Box)(({theme}) => ({
   width: '100%',
-  padding: '3px 0',
   backgroundColor: 'rgb(247, 249, 249)',
-  marginBottom: 10,
+  marginBottom: 5,
 
   '& .MuiTypography-root': {
     textAlign: 'center',
   }
 }));
+
+UnreadMessagesNotification.propTypes = {
+  item: PropTypes.object,
+}
 export default UnreadMessagesNotification;
--- src/main/java/com/twitterdan/facade/chat/response/message/PagePrivateMessageOwnerResponseMapper.java
+++ src/main/java/com/twitterdan/facade/chat/response/message/PagePrivateMessageOwnerResponseMapper.java
@@ -7,6 +7,9 @@ import com.twitterdan.facade.GeneralFacade;
 import com.twitterdan.utils.message.ForeignerMessageSeenUtil;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class PagePrivateMessageOwnerResponseMapper extends GeneralFacade<Message, PrivateMessageOwnerResponse> {
   public PagePrivateMessageOwnerResponseMapper() {
@@ -15,6 +18,14 @@ public class PagePrivateMessageOwnerResponseMapper extends GeneralFacade<Message
 
   @Override
   protected void decorateDto(PrivateMessageOwnerResponse dto, Message entity, User user) {
-    dto.setIsMessageSeen(ForeignerMessageSeenUtil.isMessageSeen(entity, user));
+    List<User> users = entity.getChat().getUsers();
+
+    Optional<User> optionalUser = users.stream().filter(u -> !u.equals(user)).findFirst();
+
+    if (optionalUser.isPresent()) {
+      dto.setIsMessageSeen(ForeignerMessageSeenUtil.isMessageSeen(entity, optionalUser.get()));
+    } else {
+      dto.setIsMessageSeen(false);
+    }
   }
 }
