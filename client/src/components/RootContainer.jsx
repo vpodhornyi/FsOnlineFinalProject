d27d2d2a1423b4ee786f3290a4e7f70c4d839896
Viktor <50486210+vpodhornyi@users.noreply.github.com>
2023-01-25 21:56:12 +0200

    dev_snackbar:  add mui snack bar

--- client/src/components/RootContainer.jsx
+++ client/src/components/RootContainer.jsx
@@ -4,7 +4,11 @@ import {styled} from "@mui/material/styles";
 const RootContainer = styled('div')(({theme}) => ({
   width: '100%',
   display: 'flex',
-  position: 'relative'
+  position: 'relative',
+
+  '& .MuiSnackbar-root': {
+    bottom: '90px'
+  }
 }));
 
 export default RootContainer;
